1fb2b413972b70c0ac91f6932acca25c389c1fac
Nenonen Leevi <leevi.nenonen@aalto.fi>
2026-04-28 11:09:39 +0300

    Added HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,14 @@
 import MovieTab from "../components/MovieTab"
+import {useState} from "react"
+
 function HomePage () {
+    // Let's use useState to handle searching
+    const [Searched, setSearched] = useState("")
+
+    const Search = (e) => {
+        e.preventDefault()
+    }
+
     //let's create dummy movies to use for testing, TODO: Remove dummy movies later
     const movies = [
         {id: 1, title: "Where the wind blows", releasedate: "2027", url: "src/assets/G9tATCeXQAASkPj.jpg"},
@@ -9,9 +18,15 @@ function HomePage () {
 
     return (
     <div className="home-page">
+        <form onSubmit={Search} className="search-box">
+            <input type="text" placeholder="Search for movies..." className="search-input" value={Searched} onChange={(e) => setSearched(e.target.value)}></input>
+        </form>
+
         <div className="movie-grid">
             {/*Let's use map function to map the movie objects */}
-            {movies.map(movie => <MovieTab movie={movie} key={movie.id}/>)}
+            {movies.map((movie) => (
+            <MovieTab movie={movie} key={movie.id}/>
+            ))}
         </div>
     </div>)
 }
