75d6a858418e086177b3f293d1fdee5defaa04fb
Nenonen Leevi <leevi.nenonen@aalto.fi>
2026-04-28 11:13:53 +0300

    Added FavoritesPage and no-favorites div to it

---
+++ src/pages/FavoritesPage.jsx
@@ -0,0 +1,10 @@
+function Favorite() {
+    return (
+        <div className="no-favorites">
+            <h1>Oh no! You haven't added favorite movies yet.</h1>
+            <p>Add favorite movies by clicking the heart!</p>
+        </div>
+    )
+}
+
+export default Favorite
