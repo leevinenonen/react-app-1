566c08b1b31ab09ab644f3a29dd2cb04b135ae3a
Nenonen Leevi <leevi.nenonen@aalto.fi>
2026-04-28 12:07:35 +0300

    Added react-bootstrap package and dropdown

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -1,8 +1,19 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+//https://react-bootstrap.netlify.app/docs/components/dropdowns/
+
 function DropdownMenu() {
   return (
-  <div className="dropdown-menu">
-        Dropdown
-    </div>
-  )}
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-menu">
+        Dropdown Button
+      </Dropdown.Toggle>
 
-export default DropdownMenu
+      <Dropdown.Menu>
+        <Dropdown.Item href="/">Home</Dropdown.Item>
+        <Dropdown.Item href="/favorites">Favorites</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default DropdownMenu;
