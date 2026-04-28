f45394a1b180bc1822887e5f7f498b29597772c8
Nenonen Leevi <leevi.nenonen@aalto.fi>
2026-04-28 10:39:13 +0300

    FIX: Added unique key props for movies

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,16 +2,16 @@ import MovieTab from "../components/MovieTab"
 function HomePage () {
     //let's create dummy movies to use for testing, TODO: Remove dummy movies later
     const movies = [
-        {title: "Where the wind blows", releasedate: "2027", url: "src/assets/G9tATCeXQAASkPj.jpg"},
-        {title: "Moi123", releasedate: "1903", url: "src/assets/G9tATCeXQAASkPj.jpg"},
-        {title: "EIVITSINYTKÄVIPAHASTI", releasedate: "1933"}
+        {id: 1, title: "Where the wind blows", releasedate: "2027", url: "src/assets/G9tATCeXQAASkPj.jpg"},
+        {id: 2, title: "Moi123", releasedate: "1903", url: "src/assets/G9tATCeXQAASkPj.jpg"},
+        {id: 3, title: "EIVITSINYTKÄVIPAHASTI", releasedate: "1933"}
     ]
 
     return (
     <div className="home-page">
         <div className="movie-grid">
             {/*Let's use map function to map the movie objects */}
-            {movies.map(movie => <MovieTab movie={movie}/>)}
+            {movies.map(movie => <MovieTab movie={movie} key={movie.id}/>)}
         </div>
     </div>)
 }
