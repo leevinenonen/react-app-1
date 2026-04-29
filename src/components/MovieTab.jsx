c207bfa9fee377d3c6d6f5c775b45bd86640c553
Nenonen Leevi <leevi.nenonen@aalto.fi>
2026-04-30 00:25:26 +0300

    Added searching to home

--- src/components/MovieTab.jsx
+++ src/components/MovieTab.jsx
@@ -4,13 +4,13 @@ function MovieTab({movie}) {
     }
     return (
     <div className="movie-tab">
-        <div className="movie-img-div"><img src={movie.url} alt={movie.title}></img></div>
+        <div className="movie-img-div"><img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}></img></div>
         <div className="movie-upper-layer">
             <button className="fav-button" onClick={whenFavorited}>Fav</button>
         </div>
         <div className="movie-information">
             <h2>{movie.title}</h2>
-            <p>{movie.releasedate}</p>
+            <p>{movie.release_date}</p>
         </div>
     </div>)
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,64 @@
 import MovieTab from "../components/MovieTab"
 import {useState} from "react"
+import { searchPopularMovies } from "../services/api"
+import { searchMovies } from "../services/api"
+import {useEffect} from "react"
 
 function HomePage () {
     // Let's use useState to handle searching
     const [Searched, setSearched] = useState("")
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null)
+    const[loading, setLoading] = useState(true)
 
-    const Search = (e) => {
-        e.preventDefault()
-    }
+    const Search = async (e) => {
+        e.preventDefault();
+        if (!Searched.trim()) return
+        if (loading) return
 
-    //let's create dummy movies to use for testing, TODO: Remove dummy movies later
-    const movies = [
+        setLoading(true)
+
+        try {
+            const searchResults = await searchMovies(Searched)
+            setMovies(searchResults)
+        } catch (error) {
+            alert("Failed to load movies: " + error)
+            console.log(error)
+            setError("Failed to load movies!")
+        } finally {
+            setLoading(false)
+        }}
+
+    {/* DUMMY MOVIES FOR TESTING
+        const movies = [
         {id: 1, title: "Where the wind blows", releasedate: "2027", url: "src/assets/G9tATCeXQAASkPj.jpg"},
         {id: 2, title: "Moi123", releasedate: "1903", url: "src/assets/G9tATCeXQAASkPj.jpg"},
         {id: 3, title: "EIVITSINYTKÄVIPAHASTI", releasedate: "1933"}
-    ]
+    ]*/}
+
+    //Let's use useEffect to prevent fetching movies everytime something changes
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await searchPopularMovies()
+                setMovies(popularMovies)
+            } catch (error) {
+                alert("Failed to load movies: " + error)
+                console.log(error)
+                setError("Failed to load movies!")
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+        loadPopularMovies()
+    }, [])
 
     return (
     <div className="home-page">
         <form onSubmit={Search} className="search-box">
             <input type="text" placeholder="Search for movies..." className="search-input" value={Searched} onChange={(e) => setSearched(e.target.value)}></input>
+            <button type="submit" className="search-button">Search</button>
         </form>
 
         <div className="movie-grid">
